cf58c5ba2d601733c5f6aa8de019f5c94faebed3
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-03-27 12:32:43 -0400

    completed the get and append functions

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,5 @@
 from doubly_linked_list import DoublyLinkedList
 
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -8,33 +7,47 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
         self.len = 0
 
-
     def append(self, item):
         # first have to check if the length of the list is = to the capacity
+
         if self.len == self.capacity:
         # if it is we have to override the oldest item
-            if self.current != None: # head
-        # set the item to  the current value
-                self.current.value = item
-                self.current = self.current.value
 
-            else:
-                if self.current.next is None: # tail
-                    self.storage.tail.value = item
-                    self.current = self.storage.head
+            if self.current.next is not None:
+                self.current.value = item
+                self.current = self.current.next
+
+            elif self.current.next is None:
+                self.storage.tail.value = item
+                self.current = self.storage.head
+
 
         # now if its not at capacity
         else:
             if self.len < self.capacity:
                 self.storage.add_to_tail(item)
+                self.current = self.storage.head
                 self.len += 1
 
         # return
-        
     def get(self):
-
+        list_buffer_contents = []
+        # check to see if there is nothing in the list to get
+        if self.len is None:
+            return []
+        # now if there is something in the list to get
+        else:
+            current = self.storage.head
+            while current:
+                #append the current value to the list
+                list_buffer_contents.append(current.value)
+                #set the next to the current
+                current = current.next
 
         return list_buffer_contents
+
+
+
 
 # ----------------Stretch Goal-------------------
 
