c78aa021a314033dca6628bce89300f57d02ac23
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-03-27 12:57:43 -0400

    update with bst class

--- names/names.py
+++ names/names.py
@@ -1,5 +1,89 @@
 import time
 
+###### (Hint: You might try importing a data structure you built during the week)
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def get_max(self):
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def for_each(self,cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if  self.right:
+            self.right.for_each(cb)
+
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        # create a queue
+        q = Queue()
+        # add a root to the queue you create
+        enqueue(node)
+        # while queue is not empty (create a while loop)
+        while q.len() > 0:
+            temp = q.dequeue()
+            print(temp)
+            if temp.left:
+                q.enqueue(temp.left)
+            if temp.right:
+                q.enqueue(temp.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        stack = Stack()
+        stack.push(node)
+        while stack.len() > 0:
+            temp = stack.pop()
+            print(temp)
+            if temp.left:
+                stack.push(temp.left)
+            if temp.right:
+                stack.push(temp.right)
+# _________________________________________________________________
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
